944b4d75c5fbae133e766fa90c6ac7dda7ea83bd
Miha Lunar <mlunar@gmail.com>
2014-05-05 12:02:44 +0200

    removed pages batch and ignored

--- updatePages.bat
+++
@@ -1,9 +0,0 @@
-mkdir bin-transfer
-git checkout master
-cp -r bin/* bin-transfer/
-git checkout gh-pages
-cp -r bin-transfer/* bin/
-rm -r bin-transfer/
-git add bin/
-git commit -m "merged bin from master"
-git checkout master
