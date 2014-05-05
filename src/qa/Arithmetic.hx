2f747f015ca38af0e2c4331e81ba3a96ba02897b
Miha Lunar <mlunar@gmail.com>
2014-05-05 12:00:01 +0200

    added reals and cleaned up promotion added eval steps with printing cleaned up some stuff added debug typesetting

--- src/qa/Arithmetic.hx
+++ src/qa/Arithmetic.hx
@@ -2,6 +2,7 @@ package qa;
 import byte.ByteData;
 import hxmath.BigInt;
 import qa.Arithmetic.ArithmeticError;
+import qa.Arithmetic.Real;
 import qa.Arithmetic.SimpleFraction;
 
 enum ArithmeticBinop {
@@ -20,8 +21,8 @@ enum NumberFormat {
 
 enum Constant {
 	CInteger(n:Integer, format:NumberFormat);
-	CRatio(n:SimpleFraction);
-	//CFraction(n:String);
+	CRational(n:SimpleFraction);
+	CReal(n:Real);
 }
 
 typedef RatioValue = {
@@ -39,9 +40,64 @@ enum MathError {
 	UnsupportedOperation(msg:String);
 }
 
+abstract Real(Float) from Float to Float {
+	
+	public static inline var ZERO:Real = (0:Float);
+	
+	@:from static public function fromString(s:String):Real {
+		var n:Float = Std.parseFloat(s);
+		if (Math.isNaN(n)) throw "Unable to convert from String to Float: "+s;
+		return cast(n, Real);
+	}
+	
+	@:to static public function toString(n:Real):String {
+		return ""+(n:Float);
+	}
+	
+	static inline public function negate(n:Real):Real {
+		return -(n:Float);
+	}
+	
+	static inline public function abs(n:Real):Real {
+		return n < ZERO ? n.negate() : n;
+	}
+	
+	static inline public function pow(a:Real, b:Real):Real {
+		return Math.pow((a:Float), (b:Float));
+	}
+	
+	@:op(A + B) static public function add(a:Real, b:Real):Real {
+		return a+b;
+	}
+	@:op(A - B) static public function subtract(a:Real, b:Real):Real {
+		return a-b;
+	}
+	@:op(A * B) static public function multiply(a:Real, b:Real):Real {
+		return a*b;
+	}
+	@:op(A / B) static public function divide(a:Real, b:Real):Real {
+		return a/b;
+	}
+	
+	@:op(A > B) static public function greaterThan(a:Real, b:Real):Bool {
+		return a > b;
+	}
+	@:op(A < B) static public function lessThan(a:Real, b:Real):Bool {
+		return a < b;
+	}
+	
+}
+
+
+
 abstract SimpleFraction(RatioValue) from RatioValue to RatioValue {
 	
 	public static var ZERO:SimpleFraction = ({ a: 0, b: 0 }:RatioValue);
+	
+	@:to static public function toReal(n:SimpleFraction):Real {
+		var v:RatioValue = n;
+		return (v.a:Real)/(v.b:Real);
+	}
 	
 	public function getNumerator() {
 		return this.a;
@@ -141,7 +197,7 @@ abstract SimpleFraction(RatioValue) from RatioValue to RatioValue {
 	@:op(A / B) static public function divide(a:SimpleFraction, b:SimpleFraction):SimpleFraction {
 		var va:RatioValue = a;
 		var vb:RatioValue = b;
-		if (vb.a == (0:Integer)) throw new ArithmeticError(DivisionByZero);
+		if (vb.a == ZERO) throw new ArithmeticError(DivisionByZero);
 		var r = ({
 			a: va.a*vb.b,
 			b: va.b*vb.a
@@ -151,26 +207,12 @@ abstract SimpleFraction(RatioValue) from RatioValue to RatioValue {
 	}
 }
 
-///*
+
+
 abstract Integer(Int) from Int to Int {
 	
 	public static inline var ZERO:Integer = (0:Integer);
-	
-	static inline public function negate(n:Integer):Integer {
-		return -(n:Int);
-	}
-	
-	static inline public function abs(n:Integer):Integer {
-		return n < (0:Integer) ? n.negate() : n;
-	}
-	
-	static inline public function divideInteger(a:Integer, b:Integer):Integer {
-		return Math.floor((a:Int)/(b:Int));
-	}
-	
-	static inline public function pow(a:Integer, b:Integer):Integer {
-		return Math.floor(Math.pow((a:Int), (b:Int)));
-	}
+	public static inline var ONE:Integer = (1:Integer);
 	
 	@:from static public function fromString(s:String):Integer {
 		var n:Null<Int> = Std.parseInt(s);
@@ -187,8 +229,28 @@ abstract Integer(Int) from Int to Int {
 	@:to static public function toSimpleFraction(n:Integer):SimpleFraction {
 		return ({
 			a: n,
-			b: (1:Integer)
+			b: Integer.ONE
 		}:SimpleFraction);
+	}
+	
+	static inline public function negate(n:Integer):Integer {
+		return -(n:Int);
+	}
+	
+	static inline public function abs(n:Integer):Integer {
+		return n < ZERO ? n.negate() : n;
+	}
+	
+	static inline public function divideInteger(a:Integer, b:Integer):Integer {
+		return Math.floor((a:Int)/(b:Int));
+	}
+	
+	static inline public function pow(a:Integer, b:Integer):Integer {
+		return Math.floor(Math.pow((a:Int), (b:Int)));
+	}
+	
+	@:to static public function toReal(n:Integer):Real {
+		return ((n:Int):Float);
 	}
 	
 	@:op(A + B) static public function add(a:Integer, b:Integer):Integer {
@@ -211,37 +273,9 @@ abstract Integer(Int) from Int to Int {
 		return a < b;
 	}
 	
-	/*
-	static public function negate(n:Decimal):Decimal {
-		var s:String = n;
-		if (s.charAt(0) == "-") return s.substr(1);
-		return "-"+s;
-	}
-	*/
-	
-	/*
-	@:from static public function fromInt(n:Int):Decimal {
-		return ""+n;
-	}
-	
-	@:op(A + B)
-	static public function add(a:Decimal, b:Decimal):Decimal {
-		return ""+(Std.parseInt(a)+Std.parseInt(b));
-	}
-	
-	@:op(A - B) static public function subtract(a:Decimal, b:Decimal):Decimal {
-		return ""+(Std.parseInt(a)-Std.parseInt(b));
-	}
-	@:op(A * B) static public function multiply(a:Decimal, b:Decimal):Decimal {
-		return ""+(Std.parseInt(a)*Std.parseInt(b));
-	}
-	@:op(A / B) static public function divide(a:Decimal, b:Decimal):Decimal {
-		return ""+(Std.parseInt(a)/Std.parseInt(b));
-	}
-	*/
-	
 }
-//*/
+
+
 
 enum ArithmeticToken {
 	TConst(c:Constant);
@@ -259,6 +293,21 @@ enum ArithmeticExpr {
 }
 
 class ArithmeticPrinter {
+	static public function printEvalStep(step:EvalStep) {
+		return switch (step) {
+			case Promote(c, p): 'Promoted ${printConstant(c)} to ${printConstant(p)}';
+		}
+	}
+	static public function printConstant(c:Constant):String {
+		var type = switch (c) {
+			case CInteger(_, _): "integer";
+			case CRational(_):   "rational";
+			case CReal(_):       "real";
+		};
+		return '$type \\(' + printTexMath(EConst(c)) + '\\)';
+		//return "\\(" + printTexMath(EConst(c)) + '^{$type} \\)';
+	}
+	
 	static public function printTex(expr:ArithmeticExpr):String {
 		return "$$"+printTexMath(expr)+"$$";
 	}
@@ -272,12 +321,12 @@ class ArithmeticPrinter {
 							//case Hexadecimal: "\\mathtt{0x"+StringTools.hex(n, 0)+"}";
 							case Hexadecimal: "\\mathtt{"+StringTools.hex(n, 0)+"}_{16}";
 						}
-					case CRatio(n):
+					case CRational(n):
 						//"\\frac{" + n.getNumerator() + "}{" + n.getDenominator() + "}";
 						"\\frac{" + n.getNumerator().toString() + "}{" + n.getDenominator().toString() + "}";
+					case CReal(n):
+						"" + n.toString();
 				};
-			//case EConst(CFraction(n)):
-				//n;
 			case EBinop(op, e1, e2):
 				"";
 				var p1 = printTexMath(e1);
@@ -339,11 +388,13 @@ class ArithmeticLexer extends hxparse.Lexer implements hxparse.RuleBuilder {
 	}
 	static public var tok = @:rule [
 	
-		"0x[0-9a-fA-F]+" =>  TConst(CInteger(lexer.current, Hexadecimal)),
-		"[0-9]+" => TConst(CInteger(lexer.current, Decimal)),
+		"0x[0-9a-fA-F]+" => TConst(CInteger(lexer.current, Hexadecimal)),
+		"[0-9]+"         => TConst(CInteger(lexer.current, Decimal)),
 		
-		//"[0-9]+\\.[0-9]+" => TConst(CFraction(lexer.current)),
-		//"\\.[0-9]+" => TConst(CFraction(lexer.current)),
+		"[0-9]+\\.[0-9]+"                    => TConst(CReal(lexer.current)),
+		"\\.[0-9]+"                          => TConst(CReal(lexer.current)),
+		"[0-9]+[eE][\\+\\-]?[0-9]+"          => TConst(CReal(lexer.current)),
+		"[0-9]+\\.[0-9]*[eE][\\+\\-]?[0-9]+" => TConst(CReal(lexer.current)),
 		
 		"\\(" => TPOpen,
 		"\\)" => TPClose,
@@ -385,8 +436,6 @@ class ArithmeticParser extends hxparse.Parser<ArithmeticLexer, ArithmeticToken> 
 				parseNext(ENeg(e));
 			case _:
 				parseNext(parseElement());
-			//case [TBinop(OpSub)]:
-				//parseNext(ENeg(parse()));
 		}
 		//step("postparse "+peek(0));
 		return e;
@@ -417,7 +466,6 @@ class ArithmeticParser extends hxparse.Parser<ArithmeticLexer, ArithmeticToken> 
 
 	function binop(e1:ArithmeticExpr, op:ArithmeticBinop, e2:ArithmeticExpr) {
 		return switch [e2, op] {
-			// TODO fix ENeg
 			case [EBinop(op2 = OpAdd | OpSub, e3, e4), op = OpMul | OpDiv],
 			     [EBinop(op2 = OpMul, e3, e4), op = OpDiv],
 			     [EBinop(op2, e3, e4), op = OpPow]:
@@ -430,8 +478,50 @@ class ArithmeticParser extends hxparse.Parser<ArithmeticLexer, ArithmeticToken> 
 	}
 }
 
+enum EvalStep {
+	Promote(c:Constant, p:Constant);
+}
+
 class ArithmeticEvaluator {
-	static public function eval(e:ArithmeticExpr):Constant {
+	
+	static function getConstantRank(c:Constant) {
+		return switch (c) {
+			case CInteger(_, _):  0;
+			case CRational(_): 1;
+			case CReal(_):     2;
+		}
+	}
+	
+	static function changeRank(c:Constant, base:Constant, steps:List<EvalStep>) {
+		var rb = getConstantRank(base);
+		
+		var rc = getConstantRank(c);
+		while (rc != rb) {
+			if (rc < rb) {
+				var p = promoteConst(c);
+				steps.add(Promote(c, p));
+				c = p;
+			} else {
+				throw 'Constant demotion not supported';
+			}
+			
+			var nrc = getConstantRank(c);
+			if (nrc == rc) throw 'Constant rank change error $c($rc == $nrc) $base($rb)';
+			rc = nrc;
+		};
+		
+		return c;
+	}
+	
+	static function promoteConst(c:Constant) {
+		return switch (c) {
+			case CInteger(n, _): CRational(n);
+			case CRational(n): CReal(n);
+			case _: throw 'Unimplemented rank promotion for $c';	
+		}
+	}
+	
+	static public function eval(e:ArithmeticExpr, steps:List<EvalStep>):Constant {
 		return switch(e) {
 			case EConst(c):
 				c; 
@@ -450,43 +540,61 @@ class ArithmeticEvaluator {
 							case OpAdd: CInteger(a+b, format);
 							case OpSub: CInteger(a-b, format);
 							case OpMul: CInteger(a*b, format);
-							case OpDiv: CRatio(a/b);
+							case OpDiv: CRational(a/b);
 							case OpPow: CInteger(a.pow(b), None);
 						};
 						
 					// Ratio
-					case [EConst(CRatio(a)), EConst(CRatio(b))]:
+					case [EConst(CRational(a)), EConst(CRational(b))]:
 						switch(op) {
-							case OpAdd: CRatio(a+b);
-							case OpSub: CRatio(a-b);
-							case OpMul: CRatio(a*b);
-							case OpDiv: CRatio(a/b);
-							case OpPow: CRatio(a.pow(b));
-							case _: throw 'Unimplemented binop $op $a $b';
+							case OpAdd: CRational(a+b);
+							case OpSub: CRational(a-b);
+							case OpMul: CRational(a*b);
+							case OpDiv: CRational(a/b);
+							case OpPow: CRational(a.pow(b));
+							//case _: throw 'Unimplemented binop $op $a $b';
 						};
 						
-					// Integer with Ratio
-					case [EConst(CInteger(a, _)), b = EConst(CRatio(_))]:
-						eval(EBinop(op, EConst(CRatio((a:SimpleFraction))), b));
-					case [a = EConst(CRatio(_)), EConst(CInteger(b, _))]:
-						eval(EBinop(op, a, EConst(CRatio((b:SimpleFraction)))));
+					// Real
+					case [EConst(CReal(a)), EConst(CReal(b))]:
+						switch (op) {
+							case OpAdd: CReal(a+b);
+							case OpSub: CReal(a-b);
+							case OpMul: CReal(a*b);
+							case OpDiv: CReal(a/b);
+							case OpPow: CReal(a.pow(b));
+						};
 					
+					case [EConst(a), EConst(b)]:
+						
+						// None of the cases match, perhaps one of them needs promotion
+						var ra = getConstantRank(a);
+						var rb = getConstantRank(b);
+						
+						// No? Well then I'm out of ideas!
+						if (ra == rb) throw 'Unimplemented binop $op $e1 $e2';
+						
+						// Promotion
+						eval(if (rb > ra) {
+							EBinop(op, EConst(changeRank(a, b, steps)), e2);
+						} else {
+							EBinop(op, e1, EConst(changeRank(b, a, steps)));
+						}, steps);
 					case _:
-						if (e1.match(EConst(_)) && e2.match(EConst(_))) throw 'Unimplemented binop $op $e1 $e2';
-						eval(EBinop(op, EConst(eval(e1)), EConst(eval(e2))));
+						eval(EBinop(op, EConst(eval(e1, steps)), EConst(eval(e2, steps))), steps);
 						
 				}
 			case EParenthesis(e1):
-				eval(e1);
+				eval(e1, steps);
 			case ENeg(e):
 				switch (e) {
 					case EConst(CInteger(n, format)):
 						CInteger(n.negate(), format);
-					case EConst(CRatio(n)):
-						CRatio(n.negate());
+					case EConst(CRational(n)):
+						CRational(n.negate());
 					case _:
 						if (e.match(EConst(_))) throw 'Unimplemented negation $e';
-						eval(ENeg(EConst(eval(e))));
+						eval(ENeg(EConst(eval(e, steps))), steps);
 				}
 		}
 	}
--- src/qa/Ksi.hx
+++ src/qa/Ksi.hx
@@ -30,14 +30,18 @@ class Ksi {
 		
 		parser.reset();
 		var expr = parser.parse();
-		var answer = ArithmeticEvaluator.eval(expr);
+		var evalSteps = new List<EvalStep>();
+		var answer = ArithmeticEvaluator.eval(expr, evalSteps);
 		return {
 			question: ArithmeticPrinter.printTex(expr),
 			answer: ""+ArithmeticPrinter.printTex(EConst(answer)),
 			debug:
 				expr+"<br/>" +
 				"<div class='tokens'>" + tokens + "</div>" +
-				"<div class='steps'>"+parser.steps.join("<br/>")+"</div>"
+				"<h3>Evaluation steps</h3>" +
+				"<div class='steps eval'>" + evalSteps.map(ArithmeticPrinter.printEvalStep).join("<br/>") + "</div>" +
+				"<h3>Parsing steps</h3>" +
+				"<div class='steps parser'>" + parser.steps.join("<br/>") + "</div>"
 		};
 	}
 	
